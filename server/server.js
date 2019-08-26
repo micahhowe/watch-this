d309ba277422c12400b60bb4c8c90dc88d819951
micahhowe <micahhowe182@gmail.com>
2019-08-26 14:01:46 -0600

    getting hosting set up

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path');
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
@@ -36,6 +37,10 @@ app.use(session({
   //donationController
   app.post('/api/payment', donationCtrl.pay)
  
+  //Hosting endpoint
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 massive(CONNECTION_STRING).then(dbInstance => {
   app.set('db', dbInstance)
